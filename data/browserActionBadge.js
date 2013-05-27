10b77b7f4221223e1ee79b67d520b9fc455139da
Rob Wu <gwnRob@gmail.com>
2013-05-27 22:31:20 +0200

    Badge styling: gradient mask, default values

--- data/browserActionBadge.js
+++ data/browserActionBadge.js
@@ -13,10 +13,18 @@ self.port.on('setBadgeText', function(text) {
     badgeText.hidden = !text;
 });
 self.port.on('setBadgeBackgroundColor', function(colorArray) {
-    colorArray[3] = colorArray[3] / 255; // Alpha channel convert [0,255] to [0,1]
-    let color = 'rgba(' + colorArray.join(',') + ')';
-    badgeText.style.backgroundColor = color;
+    // Default color when every digit is 0
+    if (colorArray.every(function(d) d === 0))
+        colorArray = [0xEE, 0, 0, 255];
 
+    colorArray[3] = colorArray[3] / 255; // Alpha channel convert [0,255] to [0,1]
+    function rgba() 'rgba(' + colorArray.join(',') + ')'
+
+    badgeText.style.backgroundColor = rgba();
+
+    // Darken the color
+    for (let i=0; i<3; ++i) colorArray[i] = colorArray[i] * 0.95;
+    badgeText.style.borderColor = rgba();
 });
 self.port.on('setIcon', function(url) {
     document.getElementById('button-img').src = url;
