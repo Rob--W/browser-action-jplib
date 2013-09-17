a7aba5d432ed051195fe45dd9e0ce3557327ea41
Rob Wu <gwnRob@gmail.com>
2013-09-17 11:26:40 +0200

    Automatically resize panel, like Chrome
    Note: There are edge cases where height/width changes are not detected.
    E.g. when you modify the width/height of the document with CSSOM
    (stylesheet manipulation).
    
    Because the dimensions are only checked after a DOM mutation, just
    trigger the event, e.g. by using setAttribute.

--- data/popup.js
+++ data/popup.js
@@ -7,23 +7,48 @@
 /*jshint browser:true*/
 /*globals self*/
 'use strict';
+var lastHeight = 0;
+var lastWidth = 0;
 function updatePanelDimensions() {
     let wrapper = document.documentElement;
+    let height = wrapper.scrollHeight;
+    let width = wrapper.scrollWidth;
+    if (height === lastHeight && width === lastWidth) {
+        return;
+    }
     let dimensions = {
-        height: wrapper.offsetHeight || wrapper.offsetHeight,
-        width: wrapper.offsetWidth || wrapper.scrollWidth
+        height: height,
+        width: width
     };
     self.port.emit('dimensions', dimensions);
 }
+// Monitor content changes for calculating the scrollheight.
+var deferredDimensionCheck;
+var lastChecked = 0;
+var rootObserver = new MutationObserver(function() {
+    var now = Date.now();
+    clearTimeout(deferredDimensionCheck);
+    if (now - lastChecked > 200) {
+        // Last check was over 0.2 seconds ago. Check immediately.
+        updatePanelDimensions();
+    } else {
+        deferredDimensionCheck = setTimeout(updatePanelDimensions, 50);
+    }
+    lastChecked = now;
+});
+rootObserver.observe(document.documentElement, {
+    attributes: true,
+    childList: true,
+    characterData: true,
+    subtree: true,
+    attributeOldValue: false,
+    characterDataOldValue: false
+});
+
 if (document.readyState == 'complete') {
     updatePanelDimensions();
 } else {
     document.addEventListener('DOMContentLoaded', updatePanelDimensions);
-    window.addEventListener('load', function() {
-        setTimeout(function() {
-            updatePanelDimensions();
-        }, 0);
-    });
 }
 
 const CLOSE_TOKEN = 'window.close.' + Math.random();
